0944aa644822725415beb4b9d8d335288812c700
Wooseok765 <dntjr7580@gmail.com>
2026-07-18 09:04:58 +0100

    booking(general validate method(예약날짜 기간검증)

--- bookings/serializer.py
+++ bookings/serializer.py
@@ -3,35 +3,52 @@ from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from .models import Booking
 
+
 class CreatRoomBookingSerializer(ModelSerializer):
-    check_in = serializers.DateField() # validate_check_in()의 반환값을 가진다
+    check_in = serializers.DateField()  # validate_check_in()의 반환값을 가진다
     check_out = serializers.DateField()
+
     # 모델에있는 동명의 값은 null=True이기 때문에 required인 serializer field로 덮어씌우려는것, 어차피 생성용이라 상관없음
-    
-    class Meta :
+    class Meta:
         model = Booking
         fields = (
             "check_in",
             "check_out",
             "guests",
         )
-        
+
     def validate_check_in(self, value):
-        # value는 validate data. 만약 들어온 value를 다시 반환한다면 validated된 data라는것(여기서 value는 post하며 유저가 입력한 check_in의 값)
+        # .is_valid()가 실행될 때 작동됨
         # 특정 필드를 검증하는(여기선 check_in을 검증) 커스텀 method를 만드는것, validate_까지가 reserved keyword
         now = timezone.localtime(timezone.now()).date()
         if now > value:
             raise serializers.ValidationError("Can't book in the past")
         return value
-    
+
     def validate_check_out(self, value):
         now = timezone.localtime(timezone.now()).date()
         if now > value:
             raise serializers.ValidationError("Can't check out before check in")
         return value
 
+    def validate(self, data):  # serializer로 넘어온 모든 데이터(유저가 입력한)를 검증
+        if data["check_out"] <= data["check_in"]:
+            raise serializers.ValidationError(
+                "check_in can't be later than check_out"
+            )  # 입력데이터가 규칙에 맞지않다는 경고를 띄움(400)
+
+        if Booking.objects.filter(
+            check_in__lte=data["check_out"],
+            check_out__gte=data["check_in"],
+        ).exists():
+            raise serializers.ValidationError("The room has already booked")
+        # 새로 부킹하려는 기간내에 일부라도 겹치는 날짜를 가진 booking 객체가 있을경우(특정 id의 room객체에 대해서) true를 반환함
+
+        return data
+
+
 class PublicBookingSerializer(ModelSerializer):
-    
+
     class Meta:
         model = Booking
         fields = (
--- rooms/views.py
+++ rooms/views.py
@@ -373,8 +373,17 @@ class RoomBookings(APIView):
     def post(self, request, pk):
         room = self.get_object(pk)
         serializer = CreatRoomBookingSerializer(data=request.data)
-        if serializer.is_valid():
-            return Response({'ok':True})            
+        # 유저가 보낸 데이터를 검증하고 Booking 객체를 생성하는 것, 이 직렬화 클래스로 생성된 객체를 Response(00.data) 할 경우 원하는 형태의 데이터가 테이블에 저장되지 않을 수 있다(왜냐하면 create 어쩌고 직렬화 클래스는 검증용으로 만들었기에 최소한의 field만 호출하여 사용했기때문)
+        if serializer.is_valid(): # 이 코드가 실행될 때 직렬화 클래스 내부의 validate 함수 작동함
+            booking = serializer.save(
+                room=room, # booking모델이 이미 room field를 가지고 있지만 직렬화 클래스의 필드에 room을 표기하면 사용자가 예약할 때 방 번호를 직접 입력하게된다는 의미
+                # 예약 시작을 해당 방 게시물에서 시작한다면 url에서 방 번호를 받아올 수 있게되어 유저가 직접 입력하지 않아도 적용 가능함 그래서 room field의 값을 따로 보내는것
+                user=request.user,
+                kind=Booking.KindOfBookingChoice.ROOM,
+            )
+            serializer = PublicBookingSerializer(booking)
+            # 최종적으로 입력하고싶은 형태가 정의된 직렬화를 사용
+            return Response(serializer.data) # 이때 data가 JSON으로 변환됨
         else:
             return Response(serializer.errors)
         
